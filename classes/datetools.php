825b28a4764d673110b8bd76861aa81821ed56c6
cmal <paul@cmal.info>
2016-07-09 20:36:35 +0000

    datetools now extends Carbon for more flexibility

--- classes/datetools.php
+++ classes/datetools.php
@@ -1,5 +1,8 @@
 <?php
 
+// TODO :
+// - reimplement tomorrow, yesterday and endOfWeek as they seem to be broken
+
 namespace Grav\Plugin;
 
 require_once __DIR__.'/../vendor/autoload.php';
@@ -7,30 +10,29 @@ require_once __DIR__.'/../vendor/autoload.php';
 use Grav\Common\Grav;
 use Grav\Common\GravTrait;
 use \Carbon\Carbon;
+use \Carbon\CarbonInterval;
 
-class DateTools
+use DateInterval;
+use DateTime;
+
+class DateTools extends Carbon
 {
-	use GravTrait;
+    use GravTrait;
 
-	/**
-	 * @var object Config
-	 */ 
-	protected $config;
+    /**
+     * @var object Config
+     */ 
+    protected $config;
 
-	/**
-	 * @var string Date format
-	 */
-	protected $dateFormat; 
+    /**
+     * @var string Date format
+     */
+    protected $dateFormat; 
 
-	/**
-	 * @var object Current Carbon datetime
-	 */
-	protected $now;
-
-	/**
-	 * Various on the fly dates 
-	 */
-	public $today;
+    /**
+     * Various on the fly dates 
+     */
+    public $today;
 	public $tomorrow;
 	public $yesterday;
 	public $startOfWeek;
@@ -40,180 +42,147 @@ class DateTools
 	public $startOfYear;
 	public $endOfYear;
 
-	/**
-	 * Date variables for processing from today
-	 */
-	protected $year;
-	protected $month;
-	protected $day;
-	protected $hour;
-	protected $min;
-	protected $dayOfWeek;   
-	protected $dayOfYear;   
-	protected $weekOfMonth; 
-	protected $weekOfYear;  
-	protected $daysInMonth;
+    /**
+     * Construct
+     */ 
+    public function __construct( $args )
+    {
+        // get the config
+        $this->config = $args['config'];
 
-	/**
-	 * Construct
-	 */ 
-	public function __construct( $args )
-	{
-		// get the config
-		$this->config = $args['config'];
+        // date format
+        $this->dateFormat = $this->config->get('plugins.datetools.dateFormat.default');
+        
+        parent::__construct();
+        
+        // initialize common dates
+        $this->initCommonDates();
+    }
+    
+    /**
+     * Copy config on clone
+     */    
+    public function __clone() {
+        $this->config = clone $this->config;
+    }
 
-		// date format
-		$this->dateFormat = $this->config->get('plugins.datetools.dateFormat.default');
-		// get today
-		$this->now = $this->now();
-		
-		// initialize processing vars
-		$this->initProcessingVars();
+    /**
+     * Initialize common dates
+     * 
+     * @internal
+     */ 
+    private function initCommonDates()
+    {
+        $this->today         = $this->copy()->format($this->dateFormat);
+        //TODO fix today and tomorrow()
+        //$this->tomorrow = $test->tomorrow()->format($test->dateFormat);
+        //$this->tomorrow      = $this->copy()->tomorrow()->format($this->dateFormat);
+        //$this->yesterday     = $this->copy()->yesterday()->format($this->dateFormat);
+        $this->startOfWeek   = $this->copy()->startOfWeek()->format($this->dateFormat);
+        //$this->endOfWeek = $this->copy()->endOfWeek();
+        //$this->endOfWeek     = $this->copy()->endOfWeek()->format($this->dateFormat);
+        $this->startOfMonth  = $this->copy()->startOfMonth()->format($this->dateFormat);
+        $this->endOfMonth    = $this->copy()->endOfMonth()->format($this->dateFormat);
+        $this->startOfYear   = $this->copy()->startOfYear()->format($this->dateFormat);
+        $this->endOfYear     = $this->copy()->endOfYear()->format($this->dateFormat);
+    }
 
-		// initialize common dates
-		$this->initCommonDates();
-	}
+    /**
+     * Parse a relative date
+     *
+     * @param string $string Relative Date string
+     * @return string DateTime
+     */
+    public function parseDate($string = null)
+    {
+        if ($string == null) {
+            return null;
+        }
 
-	/**
-	 * Initialize processing vars
-	 * 
-	 * @internal
-	 */ 
-	private function initProcessingVars()
-	{
-		$this->year = $this->now->year;
-		$this->month = $this->now->month;
-		$this->day = $this->now->day;
-		$this->hour = $this->now->hour;
-		$this->minute = $this->now->minute;
-		$this->dayOfWeek = $this->now->dayOfWeek;   
-		$this->dayOfYear = $this->now->dayOfYear;   
-		$this->weekOfMonth = $this->now->weekOfMonth; 
-		$this->weekOfYear = $this->now->weekOfYear;  
-		$this->daysInMonth = $this->now->daysInMonth;
-	}
+        return Carbon::parse($string);
+    }
+    
+    public function copy() {
+        return clone $this;
+    }
+    
+    // Begin endless wrappers to reinitialize the variables
+    
+    /*public function addDay($value = 1) {
+        return $this->addDays($value);
+    }
+    
+    public function addDays($value = 1) {
+        parent::addDays($value);
+        $this->initCommonDates();
+        return $this;
+    }
+    
+    public function addWeek($value = 1) {
+        return $this->addWeeks($value);
+    }
+    
+    public function addWeeks($value = 1) {
+        parent::addWeeks($value);
+        $this->initCommonDates();
+        return $this;
+    }
+    
+    public function addMonth($value = 1) {
+        return $this->addMonths($value);
+    }
+    
+    public function addMonths($value = 1) {
+        parent::addMonths($value);
+        $this->initCommonDates();
+        return $this;
+    }
+    
+    public function addYear($value = 1) {
+        return $this->addYears($value);
+    }
+    
+    public function addYears($value = 1) {
+        parent::addYears($value);
+        $this->initCommontDates();
+        return $this;
+    }*/
+    
+    public function modify($modify) {
+        $instance = parent::modify($modify);
+        $this->initCommonDates();
+        return $instance;
+    }
+    
+    /*public function endOfWeek() {
+        echo parent::$weekEndsAt - $this->dayOfWeek;
+        $instance = $this->addDays(parent::$weekEndsAt - $this->dayOfWeek);
+        $this->initCommonDates();
+        return $instance;
+    }*/
+    
 
-	/**
-	 * Initialize common dates
-	 * 
-	 * @internal
-	 */ 
-	private function initCommonDates()
-	{
-		$this->today	 	= $this->now()->format($this->dateFormat);
-		$this->tomorrow 	= $this->tomorrow()->format($this->dateFormat);
-		$this->yesterday 	= $this->yesterday()->format($this->dateFormat);
-		$this->startOfWeek 	= $this->startOfWeek()->format($this->dateFormat);
-		$this->endOfWeek 	= $this->endOfWeek()->format($this->dateFormat);
-		$this->startOfMonth = $this->startOfMonth()->format($this->dateFormat);
-		$this->endOfMonth 	= $this->endOfMonth()->format($this->dateFormat);
-		$this->startOfYear 	= $this->startOfYear()->format($this->dateFormat);
-		$this->endOfYear 	= $this->endOfYear()->format($this->dateFormat);
-	}
+    /**
+     * Get tomorrow's date
+     * 
+     * @return string DateTime
+     */
+    /*public function tomorrow()
+    {
+        return $this->addDay();
+    }
+    
+    public function yesterday() {
+        return $this->addDay(-1);
+    }*/
 
-	/**
-	 * Parse a relative date
-	 *
-	 * @param string $string Relative Date string
-	 * @return string DateTime
-	 */
-	public function parseDate($string = null)
-	{
-		if ($string == null) {
-			return null;
-		}
-
-		return Carbon::parse($string);
-	}
-
-	/**
-	 * Get today's date
-	 * 
-	 * @return string DateTime 
-	 */
-	public function now()
-	{
-		return Carbon::now();
-	}
-
-	/**
-	 * Get tomorrow's date
-	 * 
-	 * @return string DateTime
-	 */
-	public function tomorrow()
-	{
-		return Carbon::tomorrow();
-	} 
-
-	/**
-	 * Get yesterday's date
-	 * 
-	 * @return string DateTime
-	 */
-	public function yesterday()
-	{
-		return Carbon::yesterday();
-	} 
-
-	/**
-	 * Get start of week
-	 * 
-	 * @return string DateTime
-	 */
-	public function startOfWeek()
-	{
-		return Carbon::parse('last monday');
-	}
-
-	/**
-	 * Get end of week
-	 * 
-	 * @return string DateTime
-	 */ 
-	public function endOfWeek()
-	{
-		return Carbon::parse('next monday');
-	}
-
-	/**
-	 * Get start of month
-	 * 
-	 * @return string DateTime
-	 */
-	public function startOfMonth()
-	{
-		return Carbon::create($this->year, $this->month, 1, 0, 0, 0);
-	}
-
-	/**
-	 * Get end of month
-	 * 
-	 * @return string DateTime
-	 */ 
-	public function endOfMonth()
-	{
-		return Carbon::create($this->year, $this->month, $this->daysInMonth, 0, 0, 0);
-	}
-
-	/**
-	 * Get start of year
-	 * 
-	 * @return string DateTime
-	 */
-	public function startOfYear()
-	{
-		return Carbon::create($this->year, 1, 1, 0, 0, 0);
-	}
-
-	/**
-	 * Get end of year
-	 * 
-	 * @return string DateTime
-	 */ 
-	public function endOfYear()
-	{
-		return Carbon::create($this->year, 12, 31, 0, 0, 0);
-	}
-
+    /**
+     * Get yesterday's date
+     * 
+     * @return string DateTime
+     */
+    /*public function yesterday()
+    {
+        return $this->subDay();
+    }*/
 }
